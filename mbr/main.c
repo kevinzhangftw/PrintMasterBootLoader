e545d786081f67d2f899c4c4f694f38c2752e7cd
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-03-27 19:45:53 -0700

    Initial Commit

---
+++ mbr/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mbr
+//
+//  Created by Kai Zhang on 2017-03-27.
+//  Copyright © 2017 Kai Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
